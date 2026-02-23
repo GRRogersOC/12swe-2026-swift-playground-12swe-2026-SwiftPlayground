790298a6766ffd3e8db518ff61c481f00f722db2
GRRogersOC <george.robionyrogers@student.onslow.school.nz>
2026-02-23 23:10:21 +0000

    Committed egg shop

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,106 +1,135 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@main
-struct SwiftPlayground {
+import Foundation
 
-    /**
-    getUserInput checks if users inputted measurements are a positive number.
-    - If the users input is less than or equal to 0, it will request input again and specify a positive number.
-    - If the users input is not in a number format, it will request input again and specify a number.
-    */
-    static func getUserInput(question: String, successMessage: String) -> Double {
-        
-        var value: Double? = nil
+func menuChoice() -> Int {
+    print("""
+    ==== Egg Shop ====
+    1. Add eggs
+    2. Sell eggs
+    3. Show current stock
+    4. Show total eggs sold
+    5. Exit
+    Choose an option:
+    """)
 
-        repeat{
-            print(question)
-            // Checks if input is valid and it's a number
-            if let input = readLine(), let number = Double(input){  
-                // This is checking it's a positive number
-                if number <= 0 {
-                    print("Invalid measurement, please input a positive")
-                    
-                } else {
-                    value = number
-                }
-            
-            } else {
-                print("Invalid measurement, please input a number")
-                
-            }
-
-        } while value == nil
-
-        print(successMessage)
-        return value!
-    }
-
-    static func main() {
-
-        let maximumItemVoume = 2.0
-
-        // Input validation for measurements
-        let width = getUserInput(
-            question: "Enter room width",
-            successMessage: "Width accepted"
-        )
-
-        let length = getUserInput(
-            question: "Enter room length", 
-            successMessage: "Length accepted"
-        )
-
-        let height = getUserInput(
-            question: "Enter room height",
-            successMessage: "Height accepted"
-        )
-
-        // Calculate room volume and area
-        let roomArea = length * width
-        let roomVolume = roomArea * height
-
-        // Calculate Furniture volumes
-        let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-        for (index, furnitureVolume) in furnitureVolumes.enumerated () {
-            print("Furniture \(index + 1): \(furnitureVolume)m³")
-            if (furnitureVolume > maximumItemVoume) {
-                print("Oversized item detected")
-            }
-        }
-
-        // Calculate the usable volume
-        var totalFurnitureVolume = 0.0
-        for furnitureVolume in furnitureVolumes{
-            totalFurnitureVolume += furnitureVolume
-        }
-        print("Total furniture volume =\(totalFurnitureVolume)m³")
-
-        let usableroom = roomVolume - totalFurnitureVolume
-
-        print("")        // Print blank line for readability
-
-
-        // Users minimal volume
-        let minimalVolume = getUserInput(
-            question:("Enter minimal usable volume in meters cubed"),
-            successMessage:("Volume accecpted")
-        ) 
-
-        // Print final sumamry
-        print("")       // Prints blank line for easy to read summary
-
-        
-        print("Room area: \(roomArea) m²")
-        print("Room volume:\(roomVolume) m³")
-        print("Total furniture volume: \(totalFurnitureVolume) m³")
-        print("Usable room: \(usableroom) m³")
-        // Print success message if usable room is greater than users minimal volume
-        if usableroom > minimalVolume {
-            print("Usable volume is fine") 
+    print("Enter menu option")
+    if let input = readLine() {
+        if let number = Int(input){
+            print("Entered menu option \(number)")
+            return number
         } else {
-            print("Furniture takes up too much space")
+            print("Not a valid menu option")   
         }
-    }
+    }  
+    return 0
 }
 
+func readInteger(prompt: String) -> Int {
+    print(prompt)
+    var returnValue = -1
+    guard let userInput = readLine(), let number = Int(userInput), number > 0 else {
+        print("Invalid number.")
+        return readInteger(prompt: prompt)
+    }
+    returnValue = number
+    return returnValue
+}
+
+// addEggs takes the current stock of eggs in the shop, adds an amount to it
+// and returns the new current stock amount
+func addEggs(currentStock: Int, amount: Int) -> Int {
+    if amount <= 0 {
+        print("Cant add \(amount)")
+        return currentStock
+    }
+
+    if currentStock + amount >= 1000 {
+        print("Too many eggs, not enough storage")
+        return currentStock
+    }
+
+    return currentStock + amount 
+}
+// sellEggs takes the current tock of eggs in the shop, takes an amount from it
+// and returns the new stock amount
+func sellEggs(currentStock: Int, amount: Int) -> Int? {
+    if amount <= 0 {
+        print("Can't sell \(amount)")
+        return nil  
+    }
+
+    if amount > currentStock {
+        print("Cant sell more than \(currentStock) eggs")
+        return nil
+    }
+    return currentStock - amount
+}
+// updateSoldCount takes the current number of eggs sold, adds an amount to it
+// and returns the new amount of sold eggs
+func updateSoldCount(currentSold: Int, amount: Int) -> Int {
+    if amount <= 0 {
+        print("Cant add \(amount)")
+        return currentSold
+    }
+    return currentSold + amount
+}
+// stockMessage takes the amount of stock, converts it to a string
+// and returns the stock
+func stockMessage(stock: Int) -> String {
+    return "You have \(stock) of eggs left"
+}
+
+
+@main
+struct SwiftPlayground {
+    static func main() {
+        let availableOptions = 1...5
+
+        var currentStock = 100
+        var currentSold = 0
+        var choice: Int
+
+        repeat { 
+            repeat {
+                print("")
+                choice = menuChoice()
+            } while !availableOptions.contains(choice)
+
+            switch choice{
+            case 1:
+                print("==== Add Eggs to current stock ====")
+                let amount = readInteger(prompt: "Enter amount of eggs you are adding to current stock")
+                currentStock = addEggs(currentStock: currentStock, amount: amount)
+
+            case 2:            
+                print("==== Sell Eggs from stock ====")
+                let amount = readInteger(prompt: "Enter amount you are trying to sell")
+
+                if let newStock = sellEggs(currentStock: currentStock, amount: amount) {
+                    currentStock = newStock
+                    currentSold = updateSoldCount(currentSold: currentSold, amount: amount)
+                    print("Sale successful!")
+                } else {
+                    print("Sale failed.")
+                }
+            case 3:
+                print("==== Current Amount of Stock ====")
+                print(stockMessage(stock: currentStock))
+
+            case 4:
+                print("==== Number of Sold Eggs ====")
+                print(currentSold)
+                
+            case 5:
+                print("==== Quit ====")
+                exit(0)
+
+            default:
+                break
+                
+            } 
+        } while choice != 5
+    }
+}
